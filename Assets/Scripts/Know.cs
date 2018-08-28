c6a09f402d9d702b60a9e520a8cd4aa865314f02
zhangyinan <zhangyinan@ettech.com.cn>
2018-08-28 18:01:13 +0800

    二叉树遍历

--- Assets/Scripts/Know.cs
+++ Assets/Scripts/Know.cs
@@ -2,6 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode (int x)
+    {
+        val = x;
+    }
+}
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode (int x)
+    {
+        val = x;
+    }
+}
+
 public class Know : MonoBehaviour {
 
    public GameObject go;
@@ -33,7 +53,22 @@ public class Know : MonoBehaviour {
 
         Debug.Log(Find(7,b) );
  
+        Debug.Log(replaceSpace("We Are Happy"));
+
+        List<ListNode> l = new List<ListNode>();
+        ListNode ln = new ListNode(0);
+        int ii = 0;
+        while(ii<10){
+            ii++;
+            ListNode ln0 = new ListNode(ii);
+            l.Add(ln0);            
+        }
+        for(int j=0;j<l.Count-1;j++){
+            
+            l[j].next = l[j+1];
+        }
         
+        Debug.Log(printListFromTailToHead(null));
 
     }
 
@@ -52,12 +87,73 @@ public class Know : MonoBehaviour {
         return false;
     }
 
+    public string replaceSpace(string str)
+    {
+        // write code here
+       string s = str.Replace(" ","20%");
+
+        return s;
+    }
+
+     // 返回从尾到头的列表值序列
+    public List<int> printListFromTailToHead(ListNode listNode)
+    {
+        List<int> list = new List<int>();
+        if(listNode==null){
+            return list;
+        }
+        
+        list.Add(listNode.val);        
+        while(listNode.next!=null){
+            listNode = listNode.next;
+            list.Add(listNode.val);
+        }
+        List<int> list0 = new List<int>();
+        for(int i=list.Count-1;i>=0;i--){
+            list0.Add(list[i]);
+        }
+
+        return list0;
+    }
+
+    ///重构二叉树
+    ///pre 前序遍历  根，左，右
+    ///tin 中序遍历  左，根，右
+    ///(后序遍历) 左，右，根
+    public TreeNode reConstructBinaryTree(int[] pre, int[] tin)
+    {
+
+        // write code here
+        return null;
+    }
+
+    public int GetLightOnCount (int count){
+        
+        List<bool> list = new List<bool>();
+        //初始化灯
+        for(int i = 0; i<count;i++){
+            list.Add(false);
+        }
+
+        for(int i = 0;i<count;i++){
+            for(int j = 0;j<count;j++){            
+
+                Debug.Log("第"+i+j+"个改变了");
+                list[i+j] = !list[i+j];
+            }
+        }
+
+        return 0;
+    }
+
     void EnAble(){
         Debug.Log("Enable");
     }
 	// Use this for initialization
 	void Start () {
         StartCoroutine(Fade());
+        
+        Debug.Log("亮着的剩几个："+GetLightOnCount(4));
 	}
 	
 	// Update is called once per frame
